f2cdef3f446fcbe8fc014d758f98ead40ed24859
Bassel <basselkanso82@gmail.com>
2020-06-23 16:48:03 +0300

    Feat: create video component
    -create folder structure in folder components
    -make props to control component width, height, username, roomname

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import VideoChatComponent from './components/videoChatComponent/VideoChatComponent';
 import './App.scss';
+
+const interfaceConfigOverwrite = { 
+  DEFAULT_REMOTE_DISPLAY_NAME: 'Fellow Coder',
+  APP_NAME: 'Dev-Collab',
+  CLOSE_PAGE_GUEST_HINT: true,
+  TOOLBAR_BUTTONS: []
+}
+
+const configOverwrite = {
+  enableUserRolesBasedOnToken: true,
+  enableWelcomePage: false
+}
 
 function App() {
   return (
-    <h1>Dev-Collab</h1>
+    <VideoChatComponent 
+      interfaceConfigOverwrite = {interfaceConfigOverwrite} 
+      configOverwrite = {configOverwrite}
+      roomName = "localhost-room"
+      displayName = "Bassel Kanso"
+      password = "123"
+      height = "85vh"
+      width = "70vw"
+    />
   );
 }
 
---
+++ src/components/videoChatComponent/VideoChatComponent.js
@@ -0,0 +1,27 @@
+import React, {useEffect} from 'react';
+import { useJitsi } from 'react-jutsu'
+
+const VideoChatComponent = (props) => {
+    const roomName = props.roomName;
+    const parentNode = 'jitsi-container';
+    const height = "100%";
+    const width = "100%";
+
+    const interfaceConfigOverwrite = props.interfaceConfigOverwrite;
+    const configOverwrite = props.configOverwrite
+    const jitsi = useJitsi({ roomName, parentNode ,height, width, interfaceConfigOverwrite, configOverwrite})
+
+    useEffect(() => {
+        if (jitsi) {
+            jitsi.addEventListener('videoConferenceJoined', () => {
+                jitsi.executeCommand('displayName', props.displayName);
+                jitsi.executeCommand('password', props.password);
+            });
+        }
+        return () => jitsi && jitsi.dispose() 
+    }, [jitsi])
+
+    return <div style={{height:props.height,width:props.width}}id={parentNode} />
+}
+
+export default VideoChatComponent;
