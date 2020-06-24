8dd92f8a5dcce4899deaa321e05dff0fa6bd8949
Bassel <basselkanso82@gmail.com>
2020-06-24 16:12:29 +0300

    feat: add icons to video chat
    -install iconify library
    -make icons functional onClick
    -store the state of icons in state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ const interfaceConfigOverwrite = {
   DEFAULT_REMOTE_DISPLAY_NAME: 'Fellow Coder',
   APP_NAME: 'Dev-Collab',
   CLOSE_PAGE_GUEST_HINT: true,
-  TOOLBAR_BUTTONS: []
+  TOOLBAR_BUTTONS: [],
+  DISPLAY_WELCOME_PAGE_CONTENT: false,
+  MOBILE_APP_PROMO: false,
+  NATIVE_APP_NAME: 'Dev-Collab',
+  SHOW_JITSI_WATERMARK: false,
+  SHOW_POWERED_BY: false,
+  SHOW_PROMOTIONAL_CLOSE_PAGE: false,
+  SHOW_WATERMARK_FOR_GUESTS: false,
 }
 
 const configOverwrite = {
@@ -16,15 +23,15 @@ const configOverwrite = {
 
 function App() {
   return (
-    <VideoChatComponent 
-      interfaceConfigOverwrite = {interfaceConfigOverwrite} 
-      configOverwrite = {configOverwrite}
-      roomName = "localhost-room"
-      displayName = "Bassel Kanso"
-      password = "123"
-      height = "85vh"
-      width = "70vw"
-    />
+    <div>
+      <VideoChatComponent 
+        interfaceConfigOverwrite = {interfaceConfigOverwrite} 
+        configOverwrite = {configOverwrite}
+        roomName = "localhost-room"
+        displayName = "Bassel Kanso"
+        password = "123"
+      />
+    </div>
   );
 }
 
--- src/components/videoChatComponent/VideoChatComponent.js
+++ src/components/videoChatComponent/VideoChatComponent.js
@@ -1,27 +1,69 @@
-import React, {useEffect} from 'react';
-import { useJitsi } from 'react-jutsu'
+import React, {useEffect, useState} from 'react';
+import './VideoChatComponent.scss';
+import { useJitsi } from 'react-jutsu';
+import { Icon } from '@iconify/react';
+import videoFilled from '@iconify/icons-carbon/video-filled';
+import videoOffFilled from '@iconify/icons-carbon/video-off-filled';
+import microphoneFilled from '@iconify/icons-carbon/microphone-filled';
+import microphoneOffFilled from '@iconify/icons-carbon/microphone-off-filled';
+import screenOff from '@iconify/icons-carbon/screen-off';
+import screenIcon from '@iconify/icons-carbon/screen';
+import phoneOffFilled from '@iconify/icons-carbon/phone-off-filled';
 
 const VideoChatComponent = (props) => {
     const roomName = props.roomName;
     const parentNode = 'jitsi-container';
     const height = "100%";
     const width = "100%";
+    const [videoState, setVideoState] = useState(true);
+    const [microphoneState, setMicrophoneState] = useState(true);
+    const [screenShareState, setScreenShareState] = useState(false);
 
     const interfaceConfigOverwrite = props.interfaceConfigOverwrite;
     const configOverwrite = props.configOverwrite
     const jitsi = useJitsi({ roomName, parentNode ,height, width, interfaceConfigOverwrite, configOverwrite})
-
+    const displayName = props.displayName;
+    const password = props.password;
     useEffect(() => {
         if (jitsi) {
             jitsi.addEventListener('videoConferenceJoined', () => {
-                jitsi.executeCommand('displayName', props.displayName);
-                jitsi.executeCommand('password', props.password);
+                jitsi.executeCommand('displayName', displayName);
+                jitsi.executeCommand('password', password);
             });
         }
         return () => jitsi && jitsi.dispose() 
-    })
+    },[jitsi])
 
-    return <div style={{height:props.height,width:props.width}}id={parentNode} />
+    const toggleMute = () => {
+        jitsi.executeCommand('toggleAudio');
+        setMicrophoneState(!microphoneState);
+    }
+
+    const toggleVideo = () => {
+        jitsi.executeCommand('toggleVideo');
+        setVideoState(!videoState);
+    }
+
+    const toggleSharescreen = () => {
+        jitsi.executeCommand('toggleShareScreen');
+        setScreenShareState(!screenShareState);
+    }
+
+    const hangUp = () => {
+        jitsi.executeCommand('hangup');
+    }
+
+    return (
+        <React.Fragment>
+            <div className="videobox" id={parentNode} />
+            <div className="toolbar">
+                <Icon className="toolbar__icon" onClick = {toggleMute} icon={microphoneState? microphoneFilled : microphoneOffFilled }/>
+                <Icon className="toolbar__icon toolbar__icon--hangup" onClick = {hangUp} icon={phoneOffFilled } />
+                <Icon className="toolbar__icon" onClick = {toggleVideo} icon={videoState ? videoFilled:videoOffFilled} />
+                <Icon className="toolbar__icon" onClick = {toggleSharescreen} icon={screenShareState ? screenIcon : screenOff}/>
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default VideoChatComponent;
