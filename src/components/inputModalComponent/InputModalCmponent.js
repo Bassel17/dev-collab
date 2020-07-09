fa6fcb4ce3fc3050d5c15a54649b314578e97502
Bassel <basselkanso82@gmail.com>
2020-07-09 10:04:51 +0300

    feat: style the home page
    -make elements respnsive and in the center
    -make a button element and style it
    -button style changes on hover

---
+++ src/components/inputModalComponent/InputModalCmponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './InputModalComponent.SCSS';
+
+function InputModalComponent(){
+    return(
+        <div className = "model-container">
+            
+        </div>
+    );
+}
+
+export default InputModalComponent;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import './Home.scss';
 
 function Home (){
-    return <div>Home</div>;
+
+    return (
+        <div className="home-container">
+            <div className="home-container__section">
+                <h1 className="home-container__section__title">
+                    Remote collaboration was never easier start your session now
+                </h1>
+                <a className="home-container__section__button" href="#input-modal-component" >Start Session</a>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
